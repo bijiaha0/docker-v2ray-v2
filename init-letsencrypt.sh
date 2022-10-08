b6bfd587c3f74fb7f04a51531d1ea42ae1b984ff
jiahaobi <jiahaobi@sohu-inc.com>
2022-10-08 15:56:15 +0800

    jhbi

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="jh.whishoutan.cn"
+domains="jhbi.whishoutan.cn"
 rsa_key_size=4096
 data_path="./data/certbot"
 email="jiahao.bee@gmail.com" # Adding a valid address is strongly recommended
